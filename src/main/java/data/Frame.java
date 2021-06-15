cf6a59e9d1f42a44961bb6b38890abad99ca83ac
Malegr0 <schoening.andreas@gmx.de>
2021-06-15 17:04:47 +0200

    added previous data

---
+++ src/main/java/data/Frame.java
@@ -0,0 +1,105 @@
+package data;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame {
+
+    JFrame frame;
+    JPanel panelCont, panelStart, panelAns, panelGT, panelSyn;
+    JButton buttonAns, buttonGT, buttonSyn, buttonSendAns, buttonSendGT, buttonSendSyn;
+
+    JTextField jtfAnsCaseID, jtfAnsKeywords, jtfAnsAnswer;
+    JLabel jlAnsCaseID, jlAnsKeywords, jlAnsAnswer;
+    JTextField jtfGTID, jtfGTGenericTerm;
+    JLabel jlGTID, jlGTGenericTerm;
+    JTextField jtfSynSynonym, jtfSynID;
+    JLabel jlSynSynonym, jlSynID;
+
+    CardLayout cl;
+
+    // TODO: add DImensions for each textfield
+    Frame() {
+        frame = new JFrame("naoImporter");
+        panelCont = new JPanel();
+        cl = new CardLayout();
+
+        //Initialize components for start
+        panelStart = new JPanel();
+        buttonAns = new JButton("Answer");
+        buttonGT = new JButton("Generic terms");
+        buttonSyn = new JButton("Synonyms");
+
+        //Initialize components for answers
+        panelAns = new JPanel();
+        jlAnsCaseID = new JLabel("CaseID:");
+        jtfAnsCaseID = new JTextField();
+        jlAnsKeywords = new JLabel("Keywords:");
+        jtfAnsKeywords = new JTextField();
+        jlAnsAnswer = new JLabel("Answer:");
+        jtfAnsAnswer = new JTextField();
+        buttonSendAns = new JButton("Send Data!");
+
+        //Initialize components for generic terms
+        panelGT = new JPanel();
+        jlGTID = new JLabel("ID");
+        jtfGTID = new JTextField();
+        jlGTGenericTerm = new JLabel("Generic term:");
+        jtfGTGenericTerm = new JTextField();
+        buttonSendGT = new JButton("Send Data!");
+
+        //Initialize components for synonyms
+        panelSyn = new JPanel();
+        jlSynSynonym = new JLabel("Synonym:");
+        jtfSynSynonym = new JTextField();
+        jlSynID = new JLabel("ID:");
+        jtfSynID = new JTextField();
+        buttonSendSyn = new JButton("Send Data!");
+
+        //Configuring each component
+        panelStart.setLayout(new FlowLayout());
+        panelAns.setLayout(new FlowLayout());
+        panelGT.setLayout(new FlowLayout());
+        panelSyn.setLayout(new FlowLayout());
+
+        //Adding component ot each panel
+        panelStart.add(buttonAns);
+        panelStart.add(buttonGT);
+        panelStart.add(buttonSyn);
+
+        panelAns.add(jlAnsCaseID);
+        panelAns.add(jtfAnsCaseID);
+        panelAns.add(jlAnsKeywords);
+        panelAns.add(jtfAnsKeywords);
+        panelAns.add(jlAnsAnswer);
+        panelAns.add(jtfAnsAnswer);
+        panelAns.add(buttonSendAns);
+
+        panelGT.add(jlGTID);
+        panelGT.add(jtfGTID);
+        panelGT.add(jlGTGenericTerm);
+        panelGT.add(jtfGTGenericTerm);
+        panelGT.add(buttonSendGT);
+
+        panelSyn.add(jlSynSynonym);
+        panelSyn.add(jtfSynSynonym);
+        panelSyn.add(jlSynID);
+        panelSyn.add(jtfSynID);
+        panelSyn.add(buttonSendSyn);
+
+        panelCont.add(panelStart, "1");
+        panelCont.add(panelAns, "2");
+        panelCont.add(panelGT, "3");
+        panelCont.add(panelSyn, "4");
+
+
+        cl.show(panelCont, "1");
+
+        frame.add(panelCont);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
---
+++ src/main/java/data/URLCreator.java
@@ -0,0 +1,92 @@
+package data;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class URLCreator {
+
+    public static final String address = getAddress();
+    public static final String answersAddress = address + "/answers";
+    public static final String genericTermsAddress = address + "/genericTerms";
+    public static final String synonymsAddress = address + "/synonyms";
+
+    public static void sendAnswer(int caseID, String keywords, String answer) throws IOException, InterruptedException {
+        Map<String, String> arguments = new HashMap<>();
+        arguments.put("caseID", Integer.toString(caseID));
+        arguments.put("keywords", keywords);
+        arguments.put("answer", answer);
+        sendPostRequest(answersAddress, arguments);
+    }
+
+    public static void sendGenericTerm(int id, String genericTerm) throws IOException, InterruptedException {
+        Map<String, String> arguments = new HashMap<>();
+        arguments.put("id", Integer.toString(id));
+        arguments.put("generic_term", genericTerm);
+        sendPostRequest(genericTermsAddress, arguments);
+    }
+
+    public static void sendSynonym(String synonym, int id) throws IOException, InterruptedException {
+        Map<String, String> arguments = new HashMap<>();
+        arguments.put("synonym", synonym);
+        arguments.put("id", Integer.toString(id));
+        sendPostRequest(synonymsAddress, arguments);
+    }
+
+    private static void sendPostRequest(String address, Map<String, String> arguments) throws IOException, InterruptedException {
+        URL url = new URL(address);
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection) con;
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+
+        StringJoiner sj = new StringJoiner("&");
+        for(Map.Entry<String, String> entry: arguments.entrySet()) {
+            sj.add(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8) + "=" +
+                    URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8));
+        }
+        byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        boolean connect = true;
+        while(connect) {
+            try {
+                http.connect();
+                connect = false;
+            } catch (BindException e) {
+                Thread.sleep(5000);
+            }
+        }
+
+        try(OutputStream os = http.getOutputStream()) {
+            os.write(out);
+        }
+
+        http.disconnect();
+    }
+
+    public static String getAddress() {
+        JSONParser parser = new JSONParser();
+        String url = null;
+        try(Reader reader = new FileReader("src/main/resources/url.json")) {
+            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+            //System.out.println(jsonObject);
+
+            url = (String) jsonObject.get("url");
+            //System.out.println(url);
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+}
